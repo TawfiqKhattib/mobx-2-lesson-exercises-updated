dbfba4b194a45e0c7527decd09e00ac333b854df
tawfiq <tawfiq_kh@yahoo.com>
2021-07-08 09:22:27 +0300

    f

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react'
+import "./style/Reservation.css";
+
 
 class Reservation extends Component {
+    constructor(){
+        super();
+        this.state={id:""};
+        this.completeRes = this.completeRes.bind(this);
+        this.seatRes = this.seatRes.bind(this);
+    }
+
+    seatRes(){
+        this.props.RestaurantStore.seatRes(this.props.res.id)
+        this.setState({id:this.props.res.id})
+    }
+
+    completeRes(){
+        this.props.RestaurantStore.completeRes(this.props.res.id)
+        this.setState({id:this.props.res.id})
+    }
     render() {
+        let reservation = this.props.res
         return (
-            <div></div>
+            <div className="conatainer">
+                <div>{reservation.name} recive for {reservation.numPeople} people</div>
+                <div>
+                    <span className={reservation.completed?"conditional":null}>
+                        {reservation.completed?"completed":"notCompleted"} </span> 
+
+                    <span className={reservation.seated?"conditional":null}>
+                        {reservation.seated?"seated":"not seated"}</span>
+                </div>
+                {reservation.completed?null:<button onClick={this.completeRes}>
+                    Complete reservation</button>}
+                {reservation.seated?null:<button onClick={this.seatRes}>
+                    set reservation</button>}
+            </div>
             //render the reservation data here
             //make sure you store the ID somewhere so you can find the reservation
             //use the class "conditional" to conditionally render completed reservations
@@ -14,4 +46,4 @@ class Reservation extends Component {
 }
 
 //inject your store here
-export default Reservation
+export default inject("RestaurantStore")(observer(Reservation))
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react'
 import ResInput from './ResInput';
+import Reservation  from './Reservation';
 
 class Restaurant extends Component{
+    constructor(){
+        super();
+        this.state={update:false};
+        this.addRes = this.addRes.bind(this);
+    }
+
+    addRes(){
+        let name = this.props.GeneralStore.name;
+        let numPeople = this.props.GeneralStore.numPeople;
+        this.props.RestaurantStore.addRes(name,numPeople);
+    }
     render () {
         return (
             <div>
                 <span>You have {this.props.RestaurantStore.openTables} open tables</span>
                 {/* Add in # of people in restaurant */}
+                <div>You have {this.props.RestaurantStore.restPopulation} people in restaurant</div>
                 {/* Add in # of completed tables with id "completedTables*/}
+                <div>You have {this.props.RestaurantStore.completedTables} completed tables</div>
                 <ResInput/>
-                <button id="addRes">Add Reservation</button>
+                <button id="addRes" onClick={this.addRes}>Add Reservation</button>
                 {/* Make the Add Reservation button work */}
                 <div className = "reservations">
                 {/* Map reservation data to Reservation components here */}
+                {this.props.RestaurantStore.reservations.map((reservation,ind) => {
+                    return(<Reservation key={`t${ind}`} res={reservation} />);
+                })}
                 </div>
             </div>
         )
